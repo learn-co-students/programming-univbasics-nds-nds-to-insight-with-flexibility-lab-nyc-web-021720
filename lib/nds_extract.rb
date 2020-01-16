7990436c4a3f0914dd195259641a2fbe92d3f25c
Robert Notwicz <rob.notwicz@gmail.com>
2020-01-16 07:32:56 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -32,25 +32,17 @@ end
 
 
 # Your code after this point
-
+#
 def movies_with_director_key(name, movies_collection)
-  # GOAL: For each Hash in an Array (movies_collection), provide a collection
-  # of movies and a directors name to the movie_with_director_name method
-  # and accumulate the returned Array of movies into a new Array that's
-  # returned by this method.
-  #
-  # INPUT:
-  # * name: A director's name
-  # * movies_collection: An Array of Hashes where each Hash represents a movie
-  #
-  # RETURN:
-  #
-  # Array of Hashes where each Hash represents a movie; however, they should all have a
-  # :director_name key. This addition can be done by using the provided
-  # movie_with_director_name method
+  movies_with = []
+  movies_collection.each do |movie|
+    movies_with << movie_with_director_name(name, movie)
+  end
+  
+  return movies_with
 end
 
-
+#
 def gross_per_studio(collection)
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
   # return a Hash that includes the total worldwide_gross of all the movies from
@@ -63,20 +55,35 @@ def gross_per_studio(collection)
   #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
+  
+  studios = {}
+  collection.each do |movie|
+    addMovieToTotal(studios, movie)
+  end
+  return studios
 end
 
-def movies_with_directors_set(source)
-  # GOAL: For each director, find their :movies Array and stick it in a new Array
-  #
-  # INPUT:
-  # * source: An Array of Hashes containing director information including
-  # :name and :movies
-  #
-  # RETURN:
-  #
-  # Array of Arrays containing all of a director's movies. Each movie will need
-  # to have a :director_name key added to it.
+#
+def addMovieToTotal(studios, movie)
+  movie_studio = movie[:studio]
+  
+  if studios[movie_studio] == nil then
+    studios[movie_studio] = 0
+  end
+  
+  studios[movie_studio] += movie[:worldwide_gross]
 end
+
+#
+def movies_with_directors_set(source)
+  movies_with = []
+  source.each do |director|
+    movies_with << movies_with_director_key(director[:name], director[:movies])
+  end
+  return movies_with
+end
+
+
 
 # ----------------    End of Your Code Region --------------------
 # Don't edit the following code! Make the methods above work with this method
